0901bbea59daca9e50e23f73c4a1b20d3694689a
vaishali023 <vaishalichaudhary011@gmail.com>
2023-11-03 12:44:37 -0400

    Feat: Add react routing

--- echogram/src/App.tsx
+++ echogram/src/App.tsx
@@ -1,10 +1,19 @@
 import './globals.css';
+import {Routes, Route} from 'react-router-dom';
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <main className='flex h-screen'>
+    <Routes>
+      {/* public routes */}
+      <Route path="/sign-in" element={<SigninForm />}/>
+            
+            
+       {/* private routes */}
+      <Route index element={<Home/>}/>
+    </Routes>
+    </main>
   )
 }
 
-export default App;
+export default App
--- echogram/src/main.tsx
+++ echogram/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDom.createRoot(document.getElementById('root')!).render(
-    <App />
+    <BrowserRouter>
+        <App />
+
+    </BrowserRouter>
 )
