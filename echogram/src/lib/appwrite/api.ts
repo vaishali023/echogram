5595f5cab4ecdf6f7e6cdc166dd6e9fc847d4a97
vaishali023 <vaishalichaudhary011@gmail.com>
2023-11-15 23:15:03 -0500

    Feat: Add logout functionality

--- echogram/src/lib/appwrite/api.ts
+++ echogram/src/lib/appwrite/api.ts
@@ -82,3 +82,12 @@ export async function getCurrentUser() {
       console.log(error);
    }
 }
+
+export async function signOutAccount() {
+   try{
+      const session = await account.deleteSession("current");
+      return session;
+   } catch(error) {
+      console.log(error);
+   }
+}
