8cef9d68836c04e29fba937201727594873a802f
vaishali023 <vaishalichaudhary011@gmail.com>
2023-11-17 21:31:02 -0500

    Feat: Create CreatePost and  add the functionality to create a post and drag and drop a file.

--- echogram/src/App.tsx
+++ echogram/src/App.tsx
@@ -2,10 +2,11 @@ import './globals.css';
 import {Routes, Route} from 'react-router-dom';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
+import UpdateProfile from './_root/pages/UpdateProfile';
 
 const App = () => {
   return (
@@ -20,6 +21,15 @@ const App = () => {
        {/* private routes */}
        <Route element={<RootLayout />}>
        <Route index element={<Home/>}/>
+       <Route path='/explore' element={<Explore />} />
+       <Route path='/saved' element={<Saved />} />
+       <Route path='/all-users' element={<AllUsers />} />
+       <Route path='/create-post' element={<CreatePost />} />
+       <Route path='/update-post/:id' element={<EditPost />} />
+       <Route path='/posts/:id' element={<PostDetails />} />
+       <Route path='/profile/:id/*' element={<Profile />} />
+       <Route path='/update-profile/:id' element={<UpdateProfile/>} />
+
        </Route>
     </Routes>
     <Toaster />
--- echogram/src/components/ui/shared/LeftSidebar.tsx
+++ echogram/src/components/ui/shared/LeftSidebar.tsx
@@ -30,15 +30,15 @@ const LeftSidebar = () => {
             </Link>
             <Link to={`/[profile/${user.id}]`}
             className='flex gap-3 items-center' >
-                <img src= {user.imageUrl || '/assets/icons/profile-placeholder.svg'}
-                alt='profile'
-                className='h-14 w-14 rounded-full' />
-                <div className='flex flex-col'>
-                    <p className='body-bold'>
-                        {user.name}
-                    </p>
-                    <p className='small-regular text-light-3'> @{user.username}</p>
-                </div>
+                 <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-14 w-14 rounded-full"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">{user.name}</p>
+              <p className="small-regular text-light-3">@{user.username}</p>
+            </div>
             </Link>
             <ul className='flex flex-col gap-6'>
                  {sidebarLinks.map((link: INavLink) => {
