667371e4bef754f8ad0030ae8fd3e6738be8671a
vaishali023 <vaishalichaudhary011@gmail.com>
2023-11-20 23:39:04 -0500

    Refractor: Update Post Details page

--- echogram/src/components/ui/shared/PostStats.tsx
+++ echogram/src/components/ui/shared/PostStats.tsx
@@ -6,12 +6,12 @@ import {useState, useEffect } from 'react'
 
 
 type PostStatsProps = {
- post:Models.Document;
+ post? :Models.Document;
  userId: string;
 }
 
 const PostStats = ({post,userId }: PostStatsProps) => {
-    const likesList = post.likes.map((user: Models.Document) => user.$id)
+    const likesList = post?.likes.map((user: Models.Document) => user.$id)
 
     const [likes, setLikes] = useState(likesList);
     const[isSaved, setIsSaved] = useState(false);
@@ -23,7 +23,7 @@ const PostStats = ({post,userId }: PostStatsProps) => {
    const {data: currentUser} = useGetCurrentUser();
      
    const savedPostRecord = currentUser?.save.find(
-    (record: Models.Document) => record.$id === post.$id
+    (record: Models.Document) => record.$id === post?.$id
   );
 
   useEffect(() => {
@@ -44,7 +44,7 @@ const PostStats = ({post,userId }: PostStatsProps) => {
     }
 
     setLikes(likesArray);
-    likePost({ postId: post.$id, likesArray });
+    likePost({ postId: post?.$id  || '', likesArray });
   };
 
 
@@ -58,7 +58,7 @@ const PostStats = ({post,userId }: PostStatsProps) => {
       return deleteSavedPost(savedPostRecord.$id);
     }
 
-    savePost({ userId: userId, postId: post.$id });
+    savePost({ userId: userId, postId: post?.$id || ''});
     setIsSaved(true);
   };
 
